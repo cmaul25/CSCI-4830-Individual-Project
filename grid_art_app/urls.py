f4d874480cb6e541c2342e5ec915e33d24fc7a71
Corbin Brandl <corbin@ip-172-31-20-114.us-east-2.compute.internal>
2026-03-02 22:34:06 +0000

    full pixel page prototype

--- grid_art_app/urls.py
+++ grid_art_app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path 
-from grid_art_app import views 
-#urls patterns
-urlpatterns = [ path('', views.home, name='home'), 
-path('grid/', views.grid, name='grid'), 
+from django.urls import path
+from . import views
+#url patterns grid is the home page
+urlpatterns = [
+    path('', views.grid, name='grid'),
+    path('update_pixel/', views.update_pixel, name='update_pixel'),
 ]
